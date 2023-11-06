3a9d9085fbadbb7f9ef2c7dc5cc38b6c84461366
IlyasMohetna <ilyas.mohetna@hotmail.com>
2023-11-06 11:08:12 +0100

    résoudre problème de permission droit pour adminsitrateur et utilisateur café

--- src/Vue/Vue_Menu_Administration.php
+++ src/Vue/Vue_Menu_Administration.php
@@ -4,23 +4,35 @@ use App\Utilitaire\Vue_Composant;
 
 class Vue_Menu_Administration extends Vue_Composant
 {
-    public function __construct( )
-    {           }
+    private $niveauAutorisation;
+
+    public function __construct($id_categorie_utilisateur){
+      $this->niveauAutorisation = $id_categorie_utilisateur;
+    }
     function donneTexte(): string
     {
-
-                return "
-             <nav id='menu'>
-              <ul id='menu-closed'> 
-                <li><a href='?case=Gerer_utilisateur'>Utilisateurs</a></li>
-                <li><a href='?case=Gerer_catalogue'>Catalogue</a></li>   
-             <li><a href='?case=Gerer_entreprisesPartenaires'>Entreprises partenaires</a></li>
-               <li><a href='?case=Gerer_Commande'>Commandes</a></li>
-            
-                <li><a href='?case=Gerer_monCompte'>Mon compte</a></li> 
-               </ul>
-            </nav> 
-";
-              
+      switch($this->niveauAutorisation){
+        case 1 :
+          return "
+          <nav id='menu'>
+           <ul id='menu-closed'> 
+             <li><a href='?case=Gerer_utilisateur'>Utilisateurs</a></li>         
+             <li><a href='?case=Gerer_monCompte'>Mon compte</a></li> 
+            </ul>
+         </nav> 
+          ";
+          break;
+        case 2 :  
+          return "
+          <nav id='menu'>
+           <ul id='menu-closed'> 
+             <li><a href='?case=Gerer_catalogue'>Catalogue</a></li>   
+          <li><a href='?case=Gerer_entreprisesPartenaires'>Entreprises partenaires</a></li>
+            <li><a href='?case=Gerer_Commande'>Commandes</a></li>
+             <li><a href='?case=Gerer_monCompte'>Mon compte</a></li> 
+            </ul>
+         </nav> 
+          ";
+      }  
     }
 }
